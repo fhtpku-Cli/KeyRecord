03745ef2af3011c990ead987a98fc274ff244202
fhtpku-Cli <294281958+fhtpku-Cli@users.noreply.github.com>
2026-09-12 15:40:34 +0800

    test(app): bridge public SwiftUI accessibility selectors

---
+++ App/KeyRecordAppTests/AXBridge.h
@@ -0,0 +1,13 @@
+#import <AppKit/AppKit.h>
+NS_ASSUME_NONNULL_BEGIN
+NSArray<NSObject *> *KRAXChildren(NSObject *node);
+NSString * _Nullable KRAXIdentifier(NSObject *node);
+NSString * _Nullable KRAXLabel(NSObject *node);
+NSString * _Nullable KRAXValue(NSObject *node);
+NSString * _Nullable KRAXRole(NSObject *node);
+NSRect KRAXFrame(NSObject *node);
+BOOL KRAXPress(NSObject *node);
+BOOL KRAXFocused(NSObject *node);
+BOOL KRAXEnabled(NSObject *node);
+BOOL KRAXFocus(NSObject *node);
+NS_ASSUME_NONNULL_END
